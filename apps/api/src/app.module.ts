cdfa55f86a7ddbe9b471a49fd56f7f7a2afee94e
AbdullahAbdelmouty <abdallah0115019@gmail.com>
2026-08-01 14:28:15 +0300

    Project bootstrap

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AuditModule } from './audit/audit.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ProjectsModule, TasksModule, AuditModule],
   controllers: [AppController],
   providers: [AppService],
 })
