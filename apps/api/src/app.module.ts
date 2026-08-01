a122dc078923227bdfdf1d780cb753c3af82a8f0
AbdullahAbdelmouty <abdallah0115019@gmail.com>
2026-08-01 17:31:21 +0300

    Configure TypeORM and database connection

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,9 +6,43 @@ import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AuditModule } from './audit/audit.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProjectsModule, TasksModule, AuditModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProjectsModule,
+    TasksModule,
+    AuditModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+
+        host: config.get<string>('database.host'),
+        port: config.get<number>('database.port'),
+
+        username: config.get<string>('database.username'),
+        password: config.get<string>('database.password'),
+
+        database: config.get<string>('database.database'),
+
+        autoLoadEntities: true,
+
+        synchronize: false,
+
+        logging: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
